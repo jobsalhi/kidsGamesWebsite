8f1e3068a25ca7ae73de73c3d3765081eeee3276
job <salhiayoub2004@gmail.com>
2024-03-31 16:00:26 +0200

    fixed side bar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,32 +1,41 @@
 import pair from "../images/pair.png";
 import ours from "../images/polar.png";
-import story from "../images/story.png"
+import story from "../images/story.png";
+import { FaArrowRight } from "react-icons/fa";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="trigger"></div>
+      <div className="trigger">
+      <FaArrowRight />
+      </div>
 
       <div className="options">
         <div className="ours">
           <a href="/">
-            {" "}
-            <img src={ours} alt="" /> <h2>Trouver l'ours</h2>{" "}
-            <p>Trouveras-tu l’ours avant que la banquise fond?</p>
+            <img src={ours} alt="" />
+            <div className="title">
+              <h2>Trouver l'ours</h2>
+              <p>Trouveras-tu l’ours avant que la banquise fond?</p>
+            </div>
           </a>
         </div>
         <div className="pairs">
           <a href="/Pairs">
-            {" "}
-            <img src={pair} alt="" /> <h2>Jeu des paires</h2>{" "}
-            <p>Essaye de trouver les animaux de la même espèce!</p>
+            <img src={pair} alt="" />
+            <div className="title">
+              <h2>Jeu des paires</h2>
+              <p>Essaye de trouver les animaux de la même espèce!</p>
+            </div>
           </a>
         </div>
         <div className="story">
           <a href="/Story">
-            
-            <img src={story} alt="" /> <h2>Histoire de Jimmy</h2>{" "}
-            <p>explorer l'aventure de Jimmy et de ses amis au pôle Nord</p>
+            <img src={story} alt="" />
+            <div className="title">
+              <h2>Histoire de Jimmy</h2>
+              <p>Explorer l'aventure de Jimmy et de ses amis au pôle Nord</p>
+            </div>
           </a>
         </div>
       </div>
